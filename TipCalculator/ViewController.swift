5555f57e9da1c296dd426edd98dd9696655b355c
BuildTools <unconfigured@null.spigotmc.org>
2019-07-02 11:53:18 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Alvin Tuo on 7/2/19.
+//  Copyright © 2019 iOSSwiftCTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
