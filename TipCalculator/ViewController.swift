016bed5e736a55932e62fb55b3eac69e85d61938
Alvin <52668092+alvintuo@users.noreply.github.com>
2022-07-13 17:14:09 -0400

    New Tip Calculator (2022)

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -3,7 +3,7 @@
 //  TipCalculator
 //
 //  Created by Alvin Tuo on 7/2/19.
-//  Copyright © 2019 iOSSwiftCTD. All rights reserved.
+//  Copyright© 2019. All rights reserved.
 //
 
 import UIKit
@@ -19,41 +19,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var percent10Button: UIButton!
     @IBOutlet weak var percent15Button: UIButton!
+    @IBOutlet weak var percent18Button: UIButton!
     @IBOutlet weak var percent20Button: UIButton!
     @IBOutlet weak var percent25Button: UIButton!
     @IBOutlet weak var currentButton: UILabel!
+    @IBOutlet weak var customPercentage: UITextField!
+    @IBOutlet weak var calculateCustom: UIButton!
+    @IBOutlet weak var roundDollarBoolean: UISwitch!
+    @IBOutlet weak var roundedLabel: UILabel!
     
     var valueOfPeople = 1.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
- 
- Following needs to be done before publication: The app should reload the tip amount without pushing, with a constant loop. This ensures that a person would not have to press a button a second time */
-//        var count = 0
-//        _ = Timer.scheduledTimer(withTimeInterval: 0.188, repeats: true){ t in
-//            count += 1
-//            if count >= 888888888888 {
-//                t.invalidate()
-//            }
-//        }
     }
-    
-    
-    
     
     @IBAction func tenPercentButton(_ sender: Any) {
         if let tenTip = Double(totalAmount.text!)  {
             let tenTipAmt = (0.1 * tenTip) / valueOfPeople
             let tenTipTotal = 1.1 * tenTip / valueOfPeople
             let tenTipRenewed = tenTip / valueOfPeople
-            let tenTipRounded = String(format: "%.2f", tenTipRenewed)
-            let tenTipAmtRounded = String(format: "%.2f", tenTipAmt)
-            let tenTipTotalRounded = String(format: "%.2f", tenTipTotal)
-            tipLabel.text = "\(tenTipRounded)"
-            totalLabel.text = "\(tenTipTotalRounded)"
-            preTipLabel.text = "\(tenTipAmtRounded)"
+            tipLabel.text = "\(String(format: "%.2f", tenTipRenewed))"
+            totalLabel.text = "\(String(format: "%.2f", tenTipTotal))"
+            preTipLabel.text = "\(String(format: "%.2f", tenTipAmt))"
             view.endEditing(true)
+            roundDollarBoolean.setOn(false, animated: true)
         }
         if let button = sender as? UIButton {
             currentButton.text = "10%"
@@ -65,16 +55,30 @@ class ViewController: UIViewController {
             let fifteenTipAmt = 0.15 * fifteenTip / valueOfPeople
             let fifteenTipTotal = 1.15 * fifteenTip / valueOfPeople
             let fifteenTipRenewed = fifteenTip / valueOfPeople
-            let fifteenTipRounded = String(format: "%.2f", fifteenTipRenewed)
-            let fifteenTipAmtRounded = String(format: "%.2f", fifteenTipAmt)
-            let fifteenTipTotalRounded = String(format: "%.2f", fifteenTipTotal)
-            tipLabel.text = "\(fifteenTipRounded)"
-            totalLabel.text = "\(fifteenTipTotalRounded)"
-            preTipLabel.text = "\(fifteenTipAmtRounded)"
+            tipLabel.text = "\(String(format: "%.2f", fifteenTipRenewed))"
+            totalLabel.text = "\(String(format: "%.2f", fifteenTipTotal))"
+            preTipLabel.text = "\(String(format: "%.2f", fifteenTipAmt))"
             view.endEditing(true)
+            roundDollarBoolean.setOn(false, animated: true)
         }
         if let button = sender as? UIButton {
             currentButton.text = "15%"
+        }
+    }
+    
+    @IBAction func eighteenPercentButton(_ sender: Any) {
+        if let eighteenTip = Double(totalAmount.text!) {
+            let eighteenTipAmt = 0.18 * eighteenTip / valueOfPeople
+            let eighteenTipTotal = 1.18 * eighteenTip / valueOfPeople
+            let eighteenTipRenewed = eighteenTip / valueOfPeople
+            tipLabel.text = "\(String(format: "%.2f", eighteenTipRenewed))"
+            totalLabel.text = "\(String(format: "%.2f", eighteenTipTotal))"
+            preTipLabel.text = "\(String(format: "%.2f", eighteenTipAmt))"
+            view.endEditing(true)
+            roundDollarBoolean.setOn(false, animated: true)
+        }
+        if let button = sender as? UIButton {
+            currentButton.text = "18%"
         }
     }
     
@@ -83,41 +87,85 @@ class ViewController: UIViewController {
             let twentyTipAmt = 0.2 * twentyTip / valueOfPeople
             let twentyTipTotal = 1.2 * twentyTip / valueOfPeople
             let twentyTipRenewed = twentyTip / valueOfPeople
-            let twentyTipRounded = String(format: "%.2f", twentyTipRenewed)
-            let twentyTipAmtRounded = String(format: "%.2f", twentyTipAmt)
-            let twentyTipTotalRounded = String(format: "%.2f", twentyTipTotal)
-            tipLabel.text = "\(twentyTipRounded)"
-            totalLabel.text = "\(twentyTipTotalRounded)"
-            preTipLabel.text = "\(twentyTipAmtRounded)"
+            tipLabel.text = "\(String(format: "%.2f", twentyTipRenewed))"
+            totalLabel.text = "\(String(format: "%.2f", twentyTipTotal))"
+            preTipLabel.text = "\(String(format: "%.2f", twentyTipAmt))"
             view.endEditing(true)
+            roundDollarBoolean.setOn(false, animated: true)
         }
         if let button = sender as? UIButton {
             currentButton.text = "20%"
         }
     }
+    
     @IBAction func twentyfivePercentButton(_ sender: Any) {
         if let twentyfiveTip = Double(totalAmount.text!) {
             let twentyfiveTipAmt = 0.25 * twentyfiveTip / valueOfPeople
             let twentyfiveTipTotal = 1.25 * twentyfiveTip / valueOfPeople
             let twentyfiveTipRenewed = twentyfiveTip / valueOfPeople
-            let twentyfiveTipRounded = String(format: "%.2f", twentyfiveTipRenewed)
-            let twentyfiveTipAmtRounded = String(format: "%.2f", twentyfiveTipAmt)
-            let twentyfiveTipTotalRounded = String(format: "%.2f", twentyfiveTipTotal)
-            tipLabel.text = "\(twentyfiveTipRounded)"
-            totalLabel.text = "\(twentyfiveTipTotalRounded)"
-            preTipLabel.text = "\(twentyfiveTipAmtRounded)"
+            tipLabel.text = "\(String(format: "%.2f", twentyfiveTipRenewed))"
+            totalLabel.text = "\(String(format: "%.2f", twentyfiveTipTotal))"
+            preTipLabel.text = "\(String(format: "%.2f", twentyfiveTipAmt))"
             view.endEditing(true)
+            roundDollarBoolean.setOn(false, animated: true)
         }
         if let button = sender as? UIButton {
             currentButton.text = "25%"
         }
     }
+
+    @IBAction func calculateCustomPercentage(_ sender: Any){
+            
+        if let billAmt = Double(totalAmount.text!) {
+            if let customTipPercent = Double(customPercentage.text!) {
+                let customTipAmt = billAmt * (customTipPercent / 100.0) / valueOfPeople
+                let customTipTotal = billAmt * ((customTipPercent / 100.0) + 1.0) / valueOfPeople
+                let customWithPeople = billAmt / valueOfPeople
+                tipLabel.text = "\(String(format: "%.2f", customWithPeople))"
+                totalLabel.text = "\(String(format: "%.2f", customTipTotal))"
+                preTipLabel.text = "\(String(format: "%.2f", customTipAmt))"
+                view.endEditing(true)
+                roundDollarBoolean.setOn(false, animated: true)
+            }
+        }
+        
+        if let button = sender as? UIButton {
+            currentButton.text = customPercentage.text! + "%"
+        }
+    }
+    
     
     
     @IBAction func numberOfPeople(_ sender: UIStepper) {
         displayPeopleResult.text = Int(sender.value).description
         valueOfPeople = sender.value
+        roundDollarBoolean.setOn(false, animated: true)
     }
+    
+    @IBAction func round(_ sender: Any) {
+        
+        if roundDollarBoolean.isOn {
+            if let pretipTotal = Double(tipLabel.text!) {
+                if let tipAmount = Double(preTipLabel.text!) {
+                    let difference = 1.0 - (pretipTotal + tipAmount).truncatingRemainder(dividingBy: 1)
+                    
+                    if difference < 1.0 {
+                    
+                    roundedLabel.text = "\(String(format: "%.2f", difference))"
+                    totalLabel.text = "\(String(format: "%.2f", (difference + pretipTotal + tipAmount)))"
+                    
+                    }
+            }
+        }
+        } else {
+            if let pretipTotal = Double(tipLabel.text!) {
+                if let tipAmount = Double(preTipLabel.text!) {
+                    roundedLabel.text = "0.00"
+                    totalLabel.text = "\(String(format: "%.2f", (pretipTotal + tipAmount)))"
+                }
+            }
+        }
+}
     
     @IBAction func updatePeople(_ sender: UIStepper) {
         if currentButton.text == String("10%") {
@@ -126,14 +174,14 @@ class ViewController: UIViewController {
             fifteenPercentButton((Any).self)
         } else if currentButton.text == String("20%") {
             twentyPercentButton((Any).self)
+        } else if currentButton.text == String("18%") {
+            eighteenPercentButton((Any).self)
         } else if currentButton.text == String("25%") {
             twentyfivePercentButton((Any).self)
+        } else {
+            calculateCustomPercentage((Any).self)
         }
-        
+        roundDollarBoolean.setOn(false, animated: true)
     }
     
-    
-    
 }
-
-
